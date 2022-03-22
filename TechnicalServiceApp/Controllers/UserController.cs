c64957953bd7fde2661b008b0e6fbc8be015533b
yagmurlu <mavibulut0106@gmail.com>
2022-03-22 22:02:13 +0300

    Tema eklendi

---
+++ TechnicalServiceApp/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TechnicalServiceApp.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        UserManager userManager = new UserManager(new EfUserDal());
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetListUser()
+        {
+            var userValues = userManager.GetList();
+            return View(userValues);
+        }
+    }
+}
