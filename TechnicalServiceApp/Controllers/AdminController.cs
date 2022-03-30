3a490a996e03bba6c3b4557cf5bbb3a1c069ecbb
yagmurlu <mavibulut0106@gmail.com>
2022-03-30 23:32:23 +0300

    Admin and user getList

--- TechnicalServiceApp/Controllers/AdminController.cs
+++ TechnicalServiceApp/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,15 @@ namespace TechnicalServiceApp.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
+        AdminManager adminManager = new AdminManager(new EfAdminDal());
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetListAdmin()
+        {
+            var adminValues = adminManager.GetList();
+            return View(adminValues);
         }
     }
 }
