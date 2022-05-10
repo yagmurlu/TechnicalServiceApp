5c236a21be1b476de00f75b47a96730532db7598
yagmurlu <mavibulut0106@gmail.com>
2022-05-10 05:06:21 +0300

    ErrorPage

---
+++ TechnicalServiceApp/Controllers/ErrorPageController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TechnicalServiceApp.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        // GET: ErrorPage
+        public ActionResult Page404()
+        {
+            Response.StatusCode = 404;
+            Response.TrySkipIisCustomErrors = true;
+            return View();
+        }
+    }
+}
